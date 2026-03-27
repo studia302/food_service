56bdc106362fc8661fa330296817d300ceae68ff
cheykrym <cheykrym@cheykrym.cheykrym>
2026-03-27 22:20:11 +0300

    add views for menu

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.db.models import Prefetch
+from rest_framework.generics import ListAPIView
+from .models import FoodCategory, Food, FoodListSerializer
 
-# Create your views here.
+
+class FoodListView(ListAPIView):
+    serializer_class = FoodListSerializer
+
+    def get_queryset(self):
+        queryset = FoodCategory.objects.filter(
+            food__is_publish=True
+        ).distinct()
+
+        queryset = queryset.prefetch_related(
+            Prefetch(
+                'food',
+                queryset=Food.objects.filter(is_publish=True).order_by('internal_code')
+            )
+        )
+
+        queryset = queryset.order_by('order_id', 'name_ru')
+        
+        return queryset
